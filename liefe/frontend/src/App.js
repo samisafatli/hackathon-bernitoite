e78de2274540adf12c2b2354332586ea47d4d5e9
Allan Assis <allan.andrade@g.globo>
2020-10-17 17:01:54 -0300

    Add Routes using react-router

--- liefe/frontend/src/App.js
+++ liefe/frontend/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import LoginForm from './components/LoginForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LoginForm/>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <LoginForm />
+          </Route>
+          <Route path="/teste">
+            <div>teste</div>
+          </Route>
+          <Route path="/">
+            <div>Home</div>
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
