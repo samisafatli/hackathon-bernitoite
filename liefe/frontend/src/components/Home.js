e57911b03661115e124b166af44f8a28ea745855
Allan Assis <allan.andrade@g.globo>
2020-10-17 21:18:47 -0300

    Change to CustomButton in Home page

--- liefe/frontend/src/components/Home.js
+++ liefe/frontend/src/components/Home.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { Button } from '@material-ui/core'
-import {
-    Link
-} from "react-router-dom";
+import CustomButton from './Button'
+import { makeStyles } from '@material-ui/core/styles'
 
-const Home = () => (
-    <div>
-        <Link to="/newDelivery">
-            <Button fullWidth variant="contained" color="primary">
-                    Nova Entrega
-            </Button>
-        </Link>
-        <Button fullWidth variant="contained" color="primary">
-            Acompanhar entrega
-        </Button>
-        <Button fullWidth variant="contained" color="primary">
-            Entregar
-        </Button>
-    </div>
-)
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        backgroundColor: "#FF8100",
+        fontWeight: 700,
+        height: 50,
+        marginBottom: 20
+    },
+}));
+
+
+const Home = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <CustomButton className={classes.button} link="newDelivery" text="Nova Entrega" />
+            <CustomButton className={classes.button} text="Acompanhar entrega" />
+            <CustomButton className={classes.button} text="Entregar" />
+        </div>
+    )
+}
 export default Home
