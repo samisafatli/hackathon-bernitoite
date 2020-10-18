77f43237f8a0f585be17d903bfffe0c7e3e9a044
Allan Assis <allan.andrade@g.globo>
2020-10-17 21:02:54 -0300

    Add some style to form 🦄

--- liefe/frontend/src/components/LoginForm.js
+++ liefe/frontend/src/components/LoginForm.js
@@ -1,28 +1,53 @@
 import React from 'react';
+import logo from '../images/Logo.png'
 import { TextField, Button } from '@material-ui/core'
-import {
-    Link
-  } from "react-router-dom";
-const LoginForm = () => (
-    <div>
-        <div>
-            Acesse sua conta
-        </div>
-        <div>
+import { makeStyles } from '@material-ui/core/styles'
+import { Link } from "react-router-dom";
+
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+      backgroundColor: "#FF8100",
+      fontWeight: 700,
+    },
+  }));
+
+const LoginForm = () => {
+    const classes = useStyles();
+
+    return (
+    <div style={{marginBottom: 40}}>
+        <header style={{marginBottom: 40}}>
+            <img src={logo}  style={{width: 150}}/>
+            <h3 style={{ margin: 10, color: "#5C307F" }}>
+                Acesse sua conta
+            </h3>
+        </header>
+        <div style={{ margin: 10 }}>
             <TextField label="Email" variant="outlined" />
         </div>
-        <div>
+        <div style={{ margin: 10 }}>
             <TextField label="Senha" variant="outlined" />
         </div>
-        <Link to="/home">
-            <Button fullWidth variant="contained" color="primary">
-                Entre
+        <div style={{ margin: 10 }}>
+            <Link to="/home">
+                <Button className={classes.button} fullWidth variant="contained" color="primary">
+                    Entre
             </Button>
-        </Link>
-        <Button fullWidth variant="contained" color="primary">
-            Cadastrar
-        </Button>
+            </Link>
+        </div>
+        <div style={{ margin: 10 }}>
+            <Button className={classes.button} fullWidth variant="contained" color="primary">
+                Cadastrar
+             </Button>
+        </div>
     </div>
-)
+    )
+}
 
 export default LoginForm
+
+
+// "#5C307F(roxo)"
+// "#BEC100(verde)"
+// "#FF8100(laranja)"
