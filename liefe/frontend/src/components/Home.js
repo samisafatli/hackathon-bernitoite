291e81258c392bfe1b1dc58bcfb7caf6f6907daf
Allan Assis <allan.andrade@g.globo>
2020-10-17 21:30:13 -0300

    Add some style to Home page 🦄

--- liefe/frontend/src/components/Home.js
+++ liefe/frontend/src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import CustomButton from './Button'
 import { makeStyles } from '@material-ui/core/styles'
-
+import Logo from '../images/LogoOrange.png'
 
 const useStyles = makeStyles((theme) => ({
     button: {
-        backgroundColor: "#FF8100",
+        backgroundColor: "#FF6e00",
         fontWeight: 700,
         height: 50,
         marginBottom: 20
@@ -18,6 +18,9 @@ const Home = () => {
 
     return (
         <div>
+            <header style={{ height: 80, width: "100%", backgroundColor: "#FF6e00", position: "fixed", top: 0, left: 0 }}>
+                <img src={Logo} style={{ width: 100, marginTop: 25 }} alt="Logo" />
+            </header>
             <CustomButton className={classes.button} link="newDelivery" text="Nova Entrega" />
             <CustomButton className={classes.button} text="Acompanhar entrega" />
             <CustomButton className={classes.button} text="Entregar" />
