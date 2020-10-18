6b1e4546cfa1408107f7ec41500402115aaece1e
Allan Assis <allan.andrade@g.globo>
2020-10-18 10:38:43 -0300

    Add logic to redirect to chat

--- liefe/frontend/src/components/TrackDelivery.js
+++ liefe/frontend/src/components/TrackDelivery.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+
 import Header from './Header'
 import TextInput from './TextInput'
 import CustomButton from './Button'
 const TrackDelivery = () => {
+    const [code, setCode] = useState()
+    let history = useHistory();
+
+    const handleClick = () => {
+        if(code){
+            history.push(`/chat/${code}`)
+        }
+    }
+
+    useEffect(() => {
+     }, [code])
     return (
         <div>
             <Header />
-            <TextInput text="Código de entrega" />
-            <CustomButton text="Confirmar" link="/buyerLoading"/>
+            <TextInput textFieldProps={{ onChange: e => setCode(e.target.value) }} text="Código de entrega" />
+            <CustomButton text="Confirmar" onClick={handleClick} />
         </div>
     )
 }
