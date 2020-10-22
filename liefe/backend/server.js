88d4719f6ef800f3df76cceb8c33b071fd5d07af
mateus.nascimento <mateus.nascimento@g.globo>
2020-10-22 11:52:29 -0300

    Add read and create routes in liefe-api

--- liefe/backend/server.js
+++ liefe/backend/server.js
@@ -1,24 +1,66 @@
 const express = require("express")
 const cors = require('cors')
-const app = express();
+const app = express()
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://liefe-admin:olx-hackaton@cluster0.dzrbk.mongodb.net/liefe?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
 app.use(express.json())
 app.use(cors())
 
-const APP_PORT = 5001;
+client.connect(err => {
+  const db = client.db("liefe");
+  const APP_PORT = 5001;
 
-app.listen(APP_PORT, () => {
-  console.log(`Liefe API Listening on port ${APP_PORT}`);
+  app.listen(APP_PORT, () => {
+    console.log(`Liefe API Listening on port ${APP_PORT}`);
+  });
+
+  app.get('/', (req, res) => {
+    res.send("Welcome to liefe-api");
+  })
+
+  app.get('/:collectionName', async (req, res) => {
+    const collection = db.collection(req.params.collectionName);
+
+    try {
+      const items = await collection.find().toArray();
+      return res.send(items);
+    } catch (err) {
+      return res.send(err);
+    }
+  })
+
+  app.post('/:collectionName', async (req, res) => {
+    const collection = db.collection(req.params.collectionName);
+    const body = req.body;
+
+    try {
+      const item = await collection.findOne({
+        email: body.email
+      })
+
+      if (item) {
+        console.log("Item already exists");
+        return await res.send({message: "Item already exits"})
+      }
+
+      const result = await collection.insertOne(body);
+
+      if (result) {
+        console.log("Item created");
+        return await res.send({message: "Item created"})
+      }
+    } catch (err) {
+      return await res.send(err)
+    }
+  })
+  // client.close();
 });
-
-app.get('/', (req, res) => {
-  res.send("Welcome to liefe-api");
-})
-
-app.post('/user', (req, res) => {
-  console.log('Bati', req.body);
-  res.send({});
-})
 
 
 const server = require("http").createServer();
