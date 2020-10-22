aa55533450954b57ed4ba5a81f13d5836e5f82b4
mateus.nascimento <mateus.nascimento@g.globo>
2020-10-22 16:44:41 -0300

    get user data by localstorage

--- liefe/frontend/src/components/Header.js
+++ liefe/frontend/src/components/Header.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../images/LogoOrange.png'
+import { getUserByEmail } from '../client/liefeClient'
 
 
-const Header = ({title}) => {
-    const user = window.localStorage.getItem("username")
+const Header = ({title = "carregando..."}) => {
+
+    const [user, setUser] = useState(null)
+
+    const onLoad = async () => {
+        const email = window.localStorage.getItem("user")
+        const userData = await getUserByEmail(email);
+        setUser(userData.username)
+    }
+
     return (
-    <header style={{ height: 80, width: "100%", backgroundColor: "#FF6e00", position: "fixed", top: 0, left: 0 }}>
+    <header onLoad={onLoad} style={{ height: 80, width: "100%", backgroundColor: "#FF6e00", position: "fixed", top: 0, left: 0 }}>
         <div style={{ width: "auto", height: "100%", display:"flex", alignItems: "center", marginRight: "10px" }}>
             <div style={{ flex: 1 }}>
                 <img src={Logo} style={{ width: 100 }} alt="Logo" />
             </div>
-            <span style={{color: "#5C307F", fontWeight: 700}}>{title || user}</span>
+            <span style={{color: "#5C307F", fontWeight: 700}}>{user || title}</span>
         </div>
     </header>
     )
