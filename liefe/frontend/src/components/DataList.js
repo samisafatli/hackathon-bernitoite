efca7745e0a4689368c4cf86886a9f3c6d7019c8
mateus.nascimento <mateus.nascimento@g.globo>
2020-10-22 17:55:18 -0300

    fix Datalist issues

--- liefe/frontend/src/components/DataList.js
+++ liefe/frontend/src/components/DataList.js
@@ -17,21 +17,21 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const categories = [
-  { name: "Autos e peças", value: "1" },
-  { name: "Para a sua casa", value: "2"},
-  { name: "Eletrônicos e celulares", value: "3"},
-  { name: "Música e Hobbies", value: "4"},
-  { name: "Esportes e lazer", value: "5"},
-  { name: "Artigos infantis", value: "6"},
-  { name: "animais de estimação", value: "7"},
-  { name: "moda e beleza", value: "8"},
-  { name: "comércio e escritório", value: "9"}
+  { name: "Autos e peças", value: 1 },
+  { name: "Para a sua casa", value: 2},
+  { name: "Eletrônicos e celulares", value: 3},
+  { name: "Música e Hobbies", value: 4},
+  { name: "Esportes e lazer", value: 5},
+  { name: "Artigos infantis", value: 6},
+  { name: "animais de estimação", value: 7},
+  { name: "moda e beleza", value: 8},
+  { name: "comércio e escritório", value: 9}
 ]
 
 export default function NativeSelects() {
   const classes = useStyles();
-  const [category, setCategory] = React.useState('');
-  const [open, setOpen] = React.useState(false);
+  const [category, setCategory] = useState('');
+  const [open, setOpen] = useState(false);
 
   const handleChange = (event) => {
     setCategory(event.target.value);
@@ -51,16 +51,18 @@ export default function NativeSelects() {
         <InputLabel id="demo-controlled-open-select-label">Categoria</InputLabel>
         <Select
           labelId="demo-controlled-open-select-label"
-          label="Categoria"
           id="demo-controlled-open-select"
+          label="Categoria"
+          value={category}
           open={open}
           onClose={handleClose}
           onOpen={handleOpen}
           onChange={handleChange}
+          displayEmpty
         >
           {
-            categories.map(category => (
-              <MenuItem value={category.value}>{category.name}</MenuItem>
+            categories.map((category, idx) => (
+              <MenuItem key={idx} value={category.value}>{category.name}</MenuItem>
             ))
           }
         </Select>
