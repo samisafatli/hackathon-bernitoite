2dd8470ab64548d70719a003fd9871367f821373
mateus.nascimento <mateus.nascimento@g.globo>
2020-10-22 13:47:09 -0300

    redirect to / on create new user

--- liefe/frontend/src/components/Register.js
+++ liefe/frontend/src/components/Register.js
@@ -1,19 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
 
-import logo from '../images/Logo.png'
-
+import Header from './Header'
 import CustomButton from './Button'
 import TextInput from './TextInput'
 
 const RegisterForm = () => {
+    const [username, setUsername] = useState(null)
+    const [usernameError, setUsernameError] = useState(null)
     const [email, setEmail] = useState(null)
-    const [password, setPassword] = useState(null)
     const [emailError, setEmailError] = useState(null)
+    const [password, setPassword] = useState(null)
     const [passwordError, setPasswordError] = useState(null)
 
+    const [submit, setSubmit] = useState(null)
+    let history = useHistory();
+
+
     const handleSubmit = (event) => {    
+        if (!username) setUsernameError("Username precisa ser preenchido");
+        else setUsernameError(false);
+
+        if (!email) setEmailError("Email precisa ser preenchido");
+        else setEmailError(false);
+
+        if (!password) setPasswordError("Senha precisa ser preenchida");
+        else setPasswordError(false);
+
+        if (!username || !email || !password) {
+            event.preventDefault();
+            return;
+        }
+
         const userData = {
+            "username": username,
             "email": email,
             "passasword": password
         }
@@ -30,22 +50,26 @@ const RegisterForm = () => {
             body: JSON.stringify(userData)
           }).then(function(response) {
             console.log(response)
+            alert("Usurário cadastrado com sucesso");
+            setSubmit(true);
             return response.json();
           });
     
         event.preventDefault();
     }
 
+    useEffect(() => {
+        if(submit){
+            history.push('/')
+        }
+    },[submit])
+
     return (
-        <div style={{ marginBottom: 40 }}>
-            <header style={{ marginBottom: 40 }}>
-                <img src={logo} style={{ width: 150 }} alt="Logo" />
-                <h3 style={{ margin: 10, color: "#5C307F" }}>
-                    Crie sua conta
-                </h3>
-            </header>
-            <TextInput error={emailError} errorMessage="Endereço de email inválido" text="Email" textFieldProps={{ onChange: e => setEmail(e.target.value) }} />
-            <TextInput error={passwordError} errorMessage="Senha inválida" text="Senha" textFieldProps={{ type: "password", onChange: e => setPassword(e.target.value) }} />
+        <div>
+            <Header title="Crie sua conta:"/>
+            <TextInput error={usernameError} errorMessage={usernameError} text="Username" textFieldProps={{ onChange: e => setUsername(e.target.value) }} />
+            <TextInput error={emailError} errorMessage={emailError} text="Email" textFieldProps={{ onChange: e => setEmail(e.target.value) }} />
+            <TextInput error={passwordError} errorMessage={passwordError} text="Senha" textFieldProps={{ type: "password", onChange: e => setPassword(e.target.value) }} />
             <CustomButton text="Criar conta" onClick={handleSubmit} />
         </div>
     )
