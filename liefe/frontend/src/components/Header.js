bc92fdf1c5dc557bf5b22bcf4a063d5721d03778
mateus.nascimento <mateus.nascimento@g.globo>
2020-10-22 13:36:22 -0300

    refactor Header styles and pass title as parameter

--- liefe/frontend/src/components/Header.js
+++ liefe/frontend/src/components/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Logo from '../images/LogoOrange.png'
 
 
-const Header = () => {
+const Header = ({title}) => {
     const user = window.localStorage.getItem("username")
-
     return (
     <header style={{ height: 80, width: "100%", backgroundColor: "#FF6e00", position: "fixed", top: 0, left: 0 }}>
-        <img src={Logo} style={{ width: 100, marginTop: 25 }} alt="Logo" />
-        <span style={{marginLeft: 160, color: "#5C307F", fontWeight: 700}}>{user}</span>
+        <div style={{ width: "auto", height: "100%", display:"flex", alignItems: "center", marginRight: "10px" }}>
+            <div style={{ flex: 1 }}>
+                <img src={Logo} style={{ width: 100 }} alt="Logo" />
+            </div>
+            <span style={{color: "#5C307F", fontWeight: 700}}>{title || user}</span>
+        </div>
     </header>
     )
 }
