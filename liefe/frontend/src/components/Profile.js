3aedce09476f0f916a91b8a71f01b0afccff515e
Allan Assis <allan.andrade@g.globo>
2020-10-23 19:27:55 -0300

    Get deliveries in Profile page

--- liefe/frontend/src/components/Profile.js
+++ liefe/frontend/src/components/Profile.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from './Header'
+import {getDeliveryByEmail} from '../client/liefeClient'
+
 const Profile = () => {
+    const email = window.localStorage.getItem("user")
+    const [deliveries, setDeliveries] = useState([])
+
+    useEffect( () => {
+        const  fetchDeliveries = async () => {
+            const deliveries = await getDeliveryByEmail(email) || []
+            console.log("devlies", deliveries)
+            setDeliveries(deliveries)
+        }
+
+        fetchDeliveries()
+    }, [])
+
     return (
         <div>
             <Header />
-            oi
+            <ul>
+                {
+                    deliveries.map(d => <div>{d._id}</div>)
+                }
+            </ul>
         </div>
     )
 }
