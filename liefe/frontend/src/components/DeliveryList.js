3befdb8ab792caf619ffe5861a4cd70dc0d2b2cd
Allan Assis <allan.andrade@g.globo>
2020-10-23 19:48:47 -0300

    Add DeliveryCard in DeliveryList

--- liefe/frontend/src/components/DeliveryList.js
+++ liefe/frontend/src/components/DeliveryList.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CodeCard from './CodeCard'
 import Header from './Header'
+import DeliveryCard from './DeliveryCard'
+import { getDeliveryByEmail } from '../client/liefeClient'
 
 const DeliveryList = () => {
+
+    const email = window.localStorage.getItem("user")
+    const [deliveries, setDeliveries] = useState([])
+
+    useEffect(() => {
+        const fetchDeliveries = async () => {
+            const deliveries = await getDeliveryByEmail(email) || []
+            console.log("devlies", deliveries)
+            setDeliveries(deliveries)
+        }
+
+        fetchDeliveries()
+    }, [])
     return (
         <div>
-            <Header/>
-            <CodeCard/>
+            <Header />
+            {deliveries.map(d => (
+                <DeliveryCard delivery={d} key={d._id}/>
+
+            ))}
         </div>
     )
 }
