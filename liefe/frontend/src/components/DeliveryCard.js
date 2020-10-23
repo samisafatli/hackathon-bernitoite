d8dc462c28b1e47143e207b06df0e98cd84c8eb4
Allan Assis <allan.andrade@g.globo>
2020-10-23 19:48:29 -0300

    Create DeliveryCard component

---
+++ liefe/frontend/src/components/DeliveryCard.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar'
+import categories from '../data/categories'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    margin: 10,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+}));
+
+const DeliveryCard = ({delivery}) =>  {
+  const classes = useStyles();
+  const category = categories.find(c => c.value === delivery.categoryIdx)
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+                <Avatar>{categories.find(c => c.value === delivery.categoryIdx).name[0]}</Avatar>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1">
+                  Categoria: {category.name}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Origem: {delivery.origin.description}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Destino: {delivery.destiny.description}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                  Remove
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
+
+export default DeliveryCard
