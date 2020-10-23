454467854b17454f18f3f75d405e1e619af49e61
mateus.nascimento <mateus.nascimento@g.globo>
2020-10-23 10:13:37 -0300

    submit deliviries on database

--- liefe/frontend/src/components/NewDelivery.js
+++ liefe/frontend/src/components/NewDelivery.js
@@ -1,23 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+
 import GoogleMaps from './GoogleMaps'
 import CustomButton from './Button'
 import Header from './Header'
 import DataList from './DataList'
+import { saveDelivery } from '../client/liefeClient'
 
 const NewDelivery = () => {
 
     const [destiny, setDestiny] = useState(null)
     const [origin, setOrigin] = useState(null)
+    const [category, setCategory] = useState(null)
     
-    console.log(origin, destiny)
+    const [submit, setSubmit] = useState(null)
+    let history = useHistory()
+
+    const handleSubmit = async () => {
+        const deliveryData = {
+            origin,
+            destiny,
+            categoryIdx: category
+        }
+        await saveDelivery(deliveryData)
+        setSubmit(true)
+    }
+
+    useEffect(() => {
+        if(submit){
+            history.push('/sellerLoading')
+        }
+    },[submit])
 
     return (
         <div>
             <Header/>
             <GoogleMaps label="Endereço de origem" setData={setOrigin}/>
             <GoogleMaps label="Endereço de destino" setData={setDestiny}/>
-            <DataList />
-            <CustomButton link="/sellerLoading" text="Buscar entregador" />
+            <DataList setData={setCategory} />
+            <CustomButton text="Buscar entregador" onClick={handleSubmit} />
         </div>
     )
 }
