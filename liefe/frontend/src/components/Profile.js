ef229ae0e4127009c9ebe3e5f226504a771664a6
Allan Assis <allan.andrade@g.globo>
2020-10-24 00:58:32 -0300

    Removing unecessary code

--- liefe/frontend/src/components/Profile.js
+++ liefe/frontend/src/components/Profile.js
@@ -1,22 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from './Header'
-import { getDeliveryByEmail } from '../client/liefeClient'
 import DeliveryList from './DeliveryList'
 
 const Profile = () => {
     const email = window.localStorage.getItem("user")
     const username = window.localStorage.getItem("username")
-    const [deliveries, setDeliveries] = useState([])
-
-    useEffect(() => {
-        const fetchDeliveries = async () => {
-            const deliveries = await getDeliveryByEmail(email) || []
-            console.log("devlies", deliveries)
-            setDeliveries(deliveries)
-        }
-
-        fetchDeliveries()
-    }, [])
 
     return (
         <div style={{ marginTop: 500 }}>
